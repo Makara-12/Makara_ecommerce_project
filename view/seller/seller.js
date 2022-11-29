ff1e7c030ca1b9aac180a6cc7a22650aa3a5d9e6
MAKARA.PHOEM <MAKARA.PHOEM@PNC.LAN>
2022-11-29 20:59:10 +0700

    Add table delete and update interface

--- view/seller/seller.js
+++ view/seller/seller.js
@@ -3,30 +3,28 @@ let demo_diload = document.querySelector('#form-dialog');
 // let sing_in = document.querySelector('.sell');
 let demo_btn_create = document.querySelector('.creatBtn')
 let table = document.querySelector('#my-table');
+let dom_btn_save_create = document.querySelector('#createEditButton')
 let dataTables = [
     {
-        id: "1",
         name: "giovanni",
         description: "red",
         price: "22",
 
     },
     {
-        id: "2",
         name: "shirt",
         description: "red",
         price: 23,
 
     },
     {
-        id: "3",
         name: "dress",
         description: "red",
         price: "24",
     },
 ]
 let indexRuning = 0;
-
+let questionToEdit = null;
 // show and hide element
 function show(element){
     element.style.display = "block"
@@ -54,74 +52,124 @@ function loadQuestions() {
 function creatTable(){
     // show(tables)
     // hide(demo_diload)
-    dom_tables = document.querySelector('#body-table');
-    dom_tables.remove()
+    // let table = document.querySelector("#my-table");
 
-    dom_tables = document.createElement("body-table");
+   document.querySelector('#body-table').remove();
+    // dom_tables.remove()
+
+    dom_tables = document.createElement("tbody");
     dom_tables.id = "body-table";
 
     table.appendChild(dom_tables);
-
-    
     for(let index = 0; index < dataTables.length; index++){
+        let tr = document.createElement('tr')
+        tr.id = index
+        // dom_tables.appendChild(tr)
+        
+    //     tr.className = "tr-table"
         let dataTable = dataTables[index]
-        // currencys = document.querySelector('#currency').value;
 
         let td1 = document.createElement('td')
         td1.id = 'idu'
-        td1.textContent = dataTable.id
+        tr.className = "tr-table"
+        td1.textContent = index+1
       
         let td2 = document.createElement('td')
         td2.id = 'name'
+        tr.className = "tr-table"
         td2.textContent = dataTable.name
        
 
         let td3 = document.createElement('td')
         td3.id = 'description'
+        tr.className = "tr-table"
         td3.textContent = dataTable.description
 
 
         let td4 = document.createElement('td')
         td4.id = 'price'
+        tr.className = "tr-table"
         td4.textContent = dataTable.price
 
         td5 = document.createElement('td')
         td5.id = 'actions'
-       
-        let editAction = document.createElement("img");
-        editAction.src = "../../img/edit.svg";
-        // editAction.addEventListener("click", editQuestion);
-        actions.appendChild(editAction);
+        tr.className = "tr-table"
 
-        let trashAction = document.createElement("img");
-        trashAction.src = "../../img/trash.png";
-        // trashAction.addEventListener("click", removeQuestion);
-        actions.appendChild(trashAction);
-        let tr = document.createElement('tr')
-        tr.id = 'tr-container'
+        // let i = document.createElement('i')
+        // i.classList.add = 'fa-pencil'
+
+        // console.log(i)
+
+        // let tr = document.createElement('tr')
+        // tr.id = 'tr-container'
+
+        
+
+     
+
+        let btn1 = document.createElement("button")
+        btn1.setAttribute("id","btn1")
+        btn1.addEventListener('click', editdDataTale)
+        btn1.textContent = 'Edit'
+
+        let btn2 = document.createElement("button")
+        btn2.setAttribute("id","btn2")
+        btn2.addEventListener('click', removeTable)
+        btn2.textContent = 'Delete'
+
+        td5.appendChild(btn1)
+        td5.appendChild(btn2)
 
         tr.appendChild(td1)
         tr.appendChild(td2)
         tr.appendChild(td3)
         tr.appendChild(td4)
         tr.appendChild(td5)
-
-
+    
         dom_tables.appendChild(tr)
-        console.log(dataTables)
-        
-        // dom_table.appendChild(table)
-
     } 
 }
 creatTable()
 
+// edite table
+function editdDataTale(event) {
+    //  Get the question index
+    questionToEdit = event.target.parentElement.parentElement.dataset.index;
+    // update the dialog with question informatin
+    let question = dataTables[questionToEdit];
+    console.log(question)
+    // document.getElementById("id").value = question.id;
+    // document.getElementById("name").value = question.name;
+    // document.getElementById("description").value = question.description;
+    // document.getElementById("price").value = question.price;
+ 
+    // Show the dialog
+    dom_btn_save_create.textContent = "EDIT";
+    show(demo_diload);
+}
+
+function removeTable(event) {
+    //  Get index
+    let index = event.target.parentElement.parentElement.dataset.index;
+
+    // Remove question
+    dataTables.splice(index, 1);
+
+    // Save to local storage
+    saveQuestions();
+    // Update the view
+    creatTable();
+  
+}
+
 // addQuestion
 // click create
-function clickOnCreatBtn(){
+function addBtn(){
     show(demo_diload)
     questionEdit = null;
+    dom_btn_save_create.textContent = "Save";
     // hide(demo_btn_create )
+    // demo_btn_create.textContent = 'Create'
 }
 function onCancel(e) {
     // TODO : when clicking on ADD button, hide the dialog
@@ -137,9 +185,8 @@ function onCreate() {
         editQuestion.name = document.getElementById('name-product').value;
         editQuestion.description = document.getElementById('descriptions').value;
         editQuestion.price = document.getElementById('prices').value;
-        // editQuestion.currency = document.getElementById('currency').value;
+     
       }else{
-        
         let newQuestion = {}
         // newQuestion.title = document.getElementById('title').value;
         newQuestion.id = document.getElementById('id').value;

4387aeb35982ea4c95f347fca33b64d3f4385cd4
MAKARA.PHOEM <MAKARA.PHOEM@PNC.LAN>
2022-11-30 20:38:12 +0700

    display product on customer.js file

--- view/customer/customer.js
+++ view/customer/customer.js
@@ -1,1 +1,75 @@
-console.log(localStorage.getItem("dataTables"));
+
+let dom_container = document.querySelector('.container');
+let dom_list_product = document.querySelector('.list-product');
+
+
+function saveProducts() { 
+    localStorage.setItem("dataTables", JSON.stringify(dataTables));
+}
+
+function loadProducts() {
+let dataTablesStorage = JSON.parse(localStorage.getItem("dataTables"));
+if (dataTablesStorage !== null) {
+    dataTables = dataTablesStorage;
+}
+};
+
+// 
+function creatProduct(){
+    loadProducts();
+    saveProducts();
+    // document.querySelector('.list-product').remove();
+    // list_product = document.createElement("div");
+    // list_product.setAttribute('class', 'list-product');
+    // dom_list_product.appendChild(list_product);
+
+    for(let dataTable of dataTables){
+        let div1 = document.createElement('div');
+        div1.setAttribute('class', 'img-product');
+
+        let img = document.createElement('img');
+        img.setAttribute('id', "product-photo");
+        img.src = dataTable.img;
+        div1.appendChild(img);
+
+        let p = document.createElement('p');
+        p.id = 'description';
+        p.textContent = dataTable.name;
+
+        let p1 = document.createElement('p');
+        p1.id = 'price';
+        p1.textContent = dataTable.price;
+ 
+        let btnBuy = document.createElement('button');
+        btnBuy.setAttribute('class', 'btn-buy');
+        btnBuy.textContent = 'buy now'
+
+        let div2 = document.createElement('div')
+        div2.setAttribute('class', 'card-product')
+        
+        div2.appendChild(div1)
+        div2.appendChild(p)
+        div2.appendChild(p1)
+        div2.appendChild(btnBuy)
+        dom_list_product .appendChild(div2)
+            
+    };
+};
+
+function searchProduct(event){
+    input_search = document.querySelector('#search').value.toUpperCase();
+
+    let inputText = document.querySelector('#description')
+    if(inputText.textContent.toLowerCase().includes(input_search) || inputText.textContent.toUpperCase().includes(input_search)){
+        inputText.parentNode.style.display = "block"
+    }else{
+        inputText.parentNode.style.display = "none"
+    }
+
+    console.log(inputText)
+
+};
+
+
+creatProduct();
+loadProducts();
--- view/seller/seller.js
+++ view/seller/seller.js
@@ -5,30 +5,37 @@ let demo_btn_create = document.querySelector('.creatBtn')
 let table = document.querySelector('#my-table');
 let dom_btn_save_create = document.querySelector('#createEditButton');
 
-let dataTables = [
-    {
-        name: "giovanni",
-        description: "red",
-        price: "22",
-        // img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Flirikamatoshi.com%2Fproducts%2Fstrawberry-midi-dress&psig=AOvVaw3QldSLanmTOTBo89TnLgDt&ust=1669857643214000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMCv087e1PsCFQAAAAAdAAAAABAE'
-
-    },
-    {
-        name: "shirt",
-        description: "red",
-        price: 23,
-        // img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Flirikamatoshi.com%2Fproducts%2Fstrawberry-midi-dress&psig=AOvVaw3QldSLanmTOTBo89TnLgDt&ust=1669857643214000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMCv087e1PsCFQAAAAAdAAAAABAE'
+// let dom_photo_product = document.getElementById('photo-product');
+// let dom_product_name = document.getElementById('product-name');
+// let dom_description = document.getElementById('descriptions');
+// let dom_price = document.getElementById('prices');
+// console.log(dom_price)
 
 
-    },
-    {
-        name: "dress",
-        description: "red",
-        price: "24",
-        // img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Flirikamatoshi.com%2Fproducts%2Fstrawberry-midi-dress&psig=AOvVaw3QldSLanmTOTBo89TnLgDt&ust=1669857643214000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMCv087e1PsCFQAAAAAdAAAAABAE'
+// let dataTables = [
+//     {
+//         name: "giovanni",
+//         description: "red",
+//         price: "22",
+//         img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCPDuKFpuuOoVQnKk3WwXLIzBuQll3EfoeKRlpMg59gw&s'
 
-    },
-];
+//     },
+//     {
+//         name: "shirt",
+//         description: "red",
+//         price: 23,
+//         img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbDi38s7ZGd2zo2pyg7RRy_VADe9qRB2611SMxsWcTJ3nTRaMHaaWvUzqf-1puZn-xSLU&usqp=CAU'
+
+
+//     },
+//     {
+//         name: "dress",
+//         description: "red",
+//         price: "24",
+//         img:'https://cf.shopee.ph/file/6e8a211a042b838768da1290ce5d380f'
+
+//     },
+// ];
 // let indexRuning = 0;
 // let questionToEdit = null;
 
@@ -42,11 +49,11 @@ function hide(element){
 };
 
 //  LOCAL STORAGE ---------------------------------------------------------
-function saveQuestions() { 
+function saveProducts() { 
         localStorage.setItem("dataTables", JSON.stringify(dataTables));
 }
 
-function loadQuestions() {
+function loadProducts() {
     let dataTablesStorage = JSON.parse(localStorage.getItem("dataTables"));
     if (dataTablesStorage !== null) {
         dataTables = dataTablesStorage;
@@ -55,64 +62,84 @@ function loadQuestions() {
 
 // loop table
 function creatTable(){
-    document.querySelector('#body-table').remove();
-    dom_tables = document.createElement("tbody");
-    dom_tables.id = "body-table";
-    table.appendChild(dom_tables);
+    loadProducts()
+    saveProducts()
 
-    for(let index = 0; index < dataTables.length; index++){
-        let tr = document.createElement('tr');
-        tr.id = index;;
+    // if (dom_photo_product.value === '' || dom_product_name.value === '' || dom_description.value === '' || dom_price.value === ''){
+    //     // confirm('need to input all')
+    // }else{
+        document.querySelector('#body-table').remove();
+        dom_tables = document.createElement("tbody");
+        dom_tables.id = "body-table";
+        table.appendChild(dom_tables);
 
-        let dataTable = dataTables[index];
-
-        let td1 = document.createElement('td');
-        td1.id = 'idu';
-        tr.className = "tr-table";
-        td1.textContent = index+1;
-        td1.setAttribute("index",index)
-      
-        let td2 = document.createElement('td');
-        td2.id = 'name';
-        tr.className = "tr-table";
-        td2.textContent = dataTable.name;
-       
-
-        let td3 = document.createElement('td');
-        td3.id = 'description';
-        tr.className = "tr-table";
-        td3.textContent = dataTable.description;
-
-        let td4 = document.createElement('td');
-        td4.id = 'price';
-        tr.className = "tr-table";
-        td4.textContent = dataTable.price;
-
-        td5 = document.createElement('td');
-        td5.id = 'actions';
-        tr.className = "tr-table";
-
-        let btn1 = document.createElement("button");
-        btn1.setAttribute("id","btn1");
-        btn1.addEventListener('click', editdDataTale);
-        btn1.textContent = 'Edit';
-
-        let btn2 = document.createElement("button");
-        btn2.setAttribute("id","btn2");
-        btn2.addEventListener('click', removeTable);
-        btn2.textContent = 'Delete';
-
-        td5.appendChild(btn1);
-        td5.appendChild(btn2);
-
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-        tr.appendChild(td5);
+        for(let index = 0; index < dataTables.length; index++){
     
-        dom_tables.appendChild(tr);
-    } ;
+            let dataTable = dataTables[index];
+    
+            let td1 = document.createElement('td');
+            td1.id = 'idu';
+    
+            td1.textContent = index+1;
+            td1.setAttribute("index",index)
+          
+            let td2 = document.createElement('td');
+            td2.id = 'name';
+            // tr.className = "tr-table";
+            td2.textContent = dataTable.name;
+           
+    
+            let td3 = document.createElement('td');
+            td3.id = 'description';
+            // tr.className = "tr-table";
+            td3.textContent = dataTable.description;
+    
+            let td4 = document.createElement('td');
+            td4.id = 'price';
+            // tr.className = "tr-table";
+            td4.textContent = dataTable.price;
+    
+            td5 = document.createElement('td');
+            td5.id = 'actions';
+            // tr.className = "tr-table";
+    
+            td6 = document.createElement('td');
+            td6.id = 'img-product';
+            // tr.className = "tr-table";
+    
+            let img = document.createElement('img')
+            img.id = "cloth"
+            img.src = dataTable.img;
+            td6.appendChild(img)
+           
+    
+            let btn1 = document.createElement("button");
+            btn1.setAttribute("id","btn1");
+            btn1.addEventListener('click', editdDataTale);
+            btn1.textContent = 'Edit';
+    
+            let btn2 = document.createElement("button");
+            btn2.setAttribute("id","btn2");
+            btn2.addEventListener('click', removeTable);
+            btn2.textContent = 'Delete';
+    
+            td5.appendChild(btn1);
+            td5.appendChild(btn2);
+    
+            let tr = document.createElement('tr');
+            tr.id = index;
+            tr.className = "tr-table";
+    
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+        
+            dom_tables.appendChild(tr);
+        };
+    // }
 };
 creatTable();
 
@@ -120,21 +147,19 @@ creatTable();
 function editdDataTale(event) {
     show(demo_diload);
     //  Get the question index
-    let ProductIndex = event.target.parentElement.parentElement.children[0].getAttribute('index');
-    ProductIndex = parseInt(ProductIndex)
-    console.log(ProductIndex)
-    // update the dialog with question informatin
-    // let getIndexQuestion = dataTables[ProductIndex];
-    // console.log(getIndexQuestion);
+    productIndex = event.target.parentElement.parentElement.children[0].getAttribute('index');
+    productIndex = parseInt(productIndex)
+    let getIndexQuestion = dataTables[productIndex];
 
-    // document.getElementById("product-name").value = question.name;
-    // console.log(document.getElementById("product-name").value = question.name)
-    // document.getElementById("descriptions").value = question.description;
-    // document.getElementById("prices").value = question.price;
+    document.getElementById("photo-product").value = getIndexQuestion.img;
+    console.log( document.getElementById("photo-product").value = getIndexQuestion.img)
+    document.getElementById("product-name").value = getIndexQuestion.name
+    document.getElementById("descriptions").value = getIndexQuestion.description;
+    document.getElementById("prices").value = getIndexQuestion.price;
  
     // Show the dialog
     dom_btn_save_create.textContent = "EDIT";
-    saveQuestions();
+    saveProducts();
 }
 
 function removeTable(event) {
@@ -145,7 +170,7 @@ function removeTable(event) {
     dataTables.splice(index, 1);
 
     // Save to local storage
-    saveQuestions();
+    saveProducts();
     // Update the view
     creatTable();
 };
@@ -154,7 +179,8 @@ function removeTable(event) {
 // click create
 function addBtn(){
     show(demo_diload);
-    // ProductIndex = null;
+    clearProduct();
+    productIndex = null;
     dom_btn_save_create.textContent = "Save";
 };
 
@@ -168,15 +194,15 @@ function onCreate() {
     hide(demo_diload);
     // hide(dom_questions_dialog);
 
-    if (ProductIndex !== null){
-        let editQuestion = ProductIndex;
-        console.log(editQuestion);
-        // editQuestion.name = document.getElementById('product-name').value;
-        // editQuestion.description = document.getElementById('descriptions').value;
-        // editQuestion.price = document.getElementById('prices').value;
-     
-      }else{
+    if (productIndex !== null){
+        let editProduct = dataTables[productIndex];
+        editProduct.img = document.getElementById("photo-product").value;
+        editProduct.name = document.getElementById('product-name').value;
+        editProduct.description = document.getElementById('descriptions').value;
+        editProduct.price = document.getElementById('prices').value;
+    }else{
         let newQuestion = {};
+        newQuestion.img = document.getElementById("photo-product").value;
         newQuestion.name = document.getElementById('product-name').value;
         newQuestion.description = document.getElementById('descriptions').value;
         newQuestion.price = document.getElementById('prices').value;
@@ -184,11 +210,19 @@ function onCreate() {
       }
 
     // 2- Save question
-    saveQuestions();
+    saveProducts();
     // 3 - Update the view
     creatTable();
 };
 
-saveQuestions();
-loadQuestions();
+// clear product
+function clearProduct(){
+    document.getElementById('photo-product').value  = "";
+    document.getElementById('product-name').value  = "";
+    document.getElementById('descriptions').value  = "";
+    document.getElementById('prices').value  = "";
+};
+
+saveProducts();
+loadProducts();
 creatTable();
